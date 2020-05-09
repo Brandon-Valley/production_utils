a897d36c5b5f22aadfc75545ec9dc8d920316c1d
Valley (US), Brandon A <brandon.a.valley@boeing.com>
2020-05-09 18:03:58 -0500

    works

---
+++ production_utils.py
@@ -0,0 +1,56 @@
+''' -- VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV -- All Utilities Standard Header -- VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV -- '''
+import sys, os    ;     sys.path.insert(1, os.path.join(sys.path[0], os.path.dirname(os.path.abspath(__file__)))) # to allow for relative imports, delete any imports under this line
+
+util_submodule_l = ['msg_box_utils']  # list of all imports from local util_submodules that could be imported elsewhere to temporarily remove from sys.modules
+
+# temporarily remove any modules that could conflict with this file's local util_submodule imports
+og_sys_modules = sys.modules    ;    pop_l = [] # save the original sys.modules to be restored at the end of this file
+for module_descrip in sys.modules.keys():  
+    if any( util_submodule in module_descrip for util_submodule in util_submodule_l )    :    pop_l.append(module_descrip) # add any module that could conflict local util_submodule imports to list to be removed from sys.modules temporarily
+for module_descrip in pop_l    :    sys.modules.pop(module_descrip) # remove all modules put in pop list from sys.modules
+util_submodule_import_check_count = 0 # count to make sure you don't add a local util_submodule import without adding it to util_submodule_l
+
+''' -- VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV -- All Utilities Standard: Local Utility Submodule Imports  -- VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV -- '''
+
+from submodules.msg_box_utils import msg_box_utils as mbu                                       ; util_submodule_import_check_count += 1
+
+''' ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ '''
+if util_submodule_import_check_count != len(util_submodule_l)    :    raise Exception("ERROR:  You probably added a local util_submodule import without adding it to the util_submodule_l")
+''' ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ '''
+    
+    
+    
+import traceback
+
+
+
+
+def show_popup_on_uncaught_exception__if__is_production(func, is_production = True):
+    
+    def show_error_msg_box(e):
+        title = 'Unspecified Exception'
+        msg = 'An uncaught exception has occurred:\n\n' + str(e)
+        
+        msg_box_result = mbu.msg_box__OK(title, msg, icon = 'stop')
+        
+        
+    try:
+        func()
+    except:
+        error_info = traceback.format_exc()
+        
+        if is_production:
+            show_error_msg_box(error_info)
+        else:
+            raise
+        
+        
+        
+    
+    
+''' -- VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV -- All Utilities Standard Footer -- VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV -- '''
+sys.modules = og_sys_modules
+''' ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ '''
+if __name__ == '__main__':
+    print('In Main:  Production_utils')
+    print('End of Main: Production_utils')
