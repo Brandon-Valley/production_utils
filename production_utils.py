3f95ca04efde4214a045eb9b313c215b2070b88e
Valley (US), Brandon A <brandon.a.valley@boeing.com>
2020-07-23 15:59:19 -0500

    allow SystemExit so sys.exit() does not raise a production error

--- production_utils.py
+++ production_utils.py
@@ -2,40 +2,48 @@ import traceback
 import subprocess
 import os
 
-if __name__ == "__main__": 
-    from   usms.msg_box_utils import msg_box_utils as mbu 
+if __name__ == "__main__":
+    from usms.msg_box_utils import msg_box_utils as mbu
 else:
-    from . usms.msg_box_utils import msg_box_utils as mbu
-    
+    from .usms.msg_box_utils import msg_box_utils as mbu
 
-APPS_ID_FILE_PATH                          = os.path.dirname(os.path.abspath(__file__)) + '\app_ids.txt'
-WRITE_APP_ID_TO_FILE__RUN_SILENT__VBS_PATH = os.path.dirname(os.path.abspath(__file__)) + '\write_app_id_to_file__run_silent.vbs'
-WRITE_APP_ID_TO_FILE__PS1_PATH             = os.path.dirname(os.path.abspath(__file__)) + '\write_app_id_to_file.ps1'
+APPS_ID_FILE_PATH = os.path.dirname(os.path.abspath(__file__)) + '\app_ids.txt'
+WRITE_APP_ID_TO_FILE__RUN_SILENT__VBS_PATH = os.path.dirname(
+    os.path.abspath(__file__)) + '\write_app_id_to_file__run_silent.vbs'
+WRITE_APP_ID_TO_FILE__PS1_PATH = os.path.dirname(os.path.abspath(__file__)) + '\write_app_id_to_file.ps1'
 
 
-def show_popup_on_uncaught_exception__if__is_production(func, is_production = True):
-    
+def show_popup_on_uncaught_exception__if__is_production(func, is_production=True):
     def show_error_msg_box(e):
         title = 'Unspecified Exception'
         msg = 'An uncaught exception has occurred:\n\n' + str(e)
-        
-        msg_box_result = mbu.msg_box__OK(title, msg, icon = 'stop')
-        
-        
+
+        msg_box_result = mbu.msg_box__OK(title, msg, icon='stop')
+
     try:
         func()
+
+    # running sys.exit() from within a program should not raise an error
+    except SystemExit:
+        pass
     except:
         error_info = traceback.format_exc()
-        
+
         if is_production:
             show_error_msg_box(error_info)
         else:
             raise
-        
-        
-        
-    
+
 
 if __name__ == '__main__':
     print('In Main:  Production_utils')
+
+
+    def exit_py():
+        import sys
+        sys.exit()
+
+
+    show_popup_on_uncaught_exception__if__is_production(exit_py, is_production=True)
+
     print('End of Main: Production_utils')
